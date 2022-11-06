4a69c38e376f98b02c20569b121bd882c4b0b02e
MolnarLorand <andrevzo84@gmail.com>
2022-11-06 13:21:50 +0200

    Lab3-Final

---
+++ Migrations/20221106103447_OrderDateInOrder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Molnar_Lorand_Lab2.Migrations
+{
+    public partial class OrderDateInOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Authors_AuthorID",
+                table: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrderDate",
+                table: "Order",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorID",
+                table: "Book",
+                column: "AuthorID",
+                principalTable: "Author",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorID",
+                table: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropColumn(
+                name: "OrderDate",
+                table: "Order");
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Authors_AuthorID",
+                table: "Book",
+                column: "AuthorID",
+                principalTable: "Authors",
+                principalColumn: "Id");
+        }
+    }
+}
